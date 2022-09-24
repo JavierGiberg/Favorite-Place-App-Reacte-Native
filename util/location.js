46a50bd9f5e42457e4c11188899424b212bab35c
https://github.com/JavierGiberg <gibergjavier@gmail.com>
2022-09-24 21:32:14 +0300

    240922_1

--- util/location.js
+++ util/location.js
@@ -1,4 +1,4 @@
-const GOOGLE_APIKEY = "AIzaSyAOZTlHgGMBWyD36W7eLdEAAdS5JXGu91o";
+const GOOGLE_APIKEY = "*******************************";
 
 export function getMapPreview(lat, lng) {
   const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:S%7C${lat},${lng}&key=${GOOGLE_APIKEY}`;
